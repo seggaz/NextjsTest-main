1e5a250845f08e03f716c36a32bedb311f4315a9
Sergii Zalyvshyi <segga17@gmail.com>
2024-06-02 20:09:35 +0300

    Fix deployment issue

--- next.config.js
+++ next.config.js
@@ -1,3 +1,19 @@
+const webpack = require('webpack')
+const nodeExternals = require('webpack-node-externals')
+
 module.exports = {
-  reactStrictMode: true,
+	webpack: (config, { isServer }) => {
+		if (isServer) {
+			config.externals = [nodeExternals()]
+		}
+
+		config.plugins.push(
+			new webpack.ProvidePlugin({
+				process: 'process/browser',
+				Buffer: ['buffer', 'Buffer'],
+			})
+		)
+
+		return config
+	},
 }
