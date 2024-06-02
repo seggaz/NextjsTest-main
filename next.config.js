ba81ee82b15d0dd2a70ed0143bd14cb85ec80f52
Sergii Zalyvshyi <segga17@gmail.com>
2024-06-02 20:22:11 +0300

    Implemented task

--- next.config.js
+++ next.config.js
@@ -1,19 +1,3 @@
-const webpack = require('webpack')
-const nodeExternals = require('webpack-node-externals')
-
 module.exports = {
-	webpack: (config, { isServer }) => {
-		if (isServer) {
-			config.externals = [nodeExternals()]
-		}
-
-		config.plugins.push(
-			new webpack.ProvidePlugin({
-				process: 'process/browser',
-				Buffer: ['buffer', 'Buffer'],
-			})
-		)
-
-		return config
-	},
+	reactStrictMode: true,
 }
